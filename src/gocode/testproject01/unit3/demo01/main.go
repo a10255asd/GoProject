c4c1849f552ee52bdb9117835c8bee71c50bbc87
刘鸡血 <1025519998@qq.com>
2023-01-12 01:10:45 +0800

    11

---
+++ src/gocode/testproject01/unit3/demo01/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// + 加号 1、正数 2、相加操作 3、字符串拼接
+	var n1 int = +10
+	fmt.Println(n1)
+
+	var n2 int = 4 + 7
+	fmt.Println(n2)
+
+	var s1 string = "abc" + "def"
+	fmt.Println(s1)
+}
